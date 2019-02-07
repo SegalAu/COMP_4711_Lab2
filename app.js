6238d462b314a6926ae3ef039e7a666f09b2ea1b
Segal Au <au.segal@gmail.com>
2019-02-06 18:17:39 -0800

    initial commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ http.createServer(function (request, response){
     let date = new Date();
     let timestamp = date.getTime();
     response.writeHead(200, {'Content-type' : 'text/plain'});
-    response.write(timestamp + " ");
+    response.write(date + " ");
     response.end(); 
 }).listen(8080); 
 
